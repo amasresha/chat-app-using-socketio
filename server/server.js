7acaa5eaba747e113b988ad398d2226f31585ee2
Masresha <family@10.30.237.105>
2017-10-03 18:27:12 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var path = require("path");
+var express =require("express");
+const app =express();
+
+var publiPath = path.join(__dirname, '../public')
+const port =process.env.PORT || 3000;
+// console.log(publicDir);
+
+ // app.get(`/`, (req,res)=>{
+ //   res.render (`${publicDir}`);
+ // })
+app.use (express.static(publiPath));
+
+app.listen(port, ()=>{
+  console.log(`You express server is running on ${port}`);
+})
