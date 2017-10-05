4260fc47705f2fb7a4904e6640eb4e4b8f8958b4
amasresha <a.masresha@gmail.com>
2017-10-05 11:17:58 -0700

    add socket io

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 var path = require("path");
 var express =require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const app =express();
+var server= http.createServer(app);
+var io=socketIO(server);
 
 var publiPath = path.join(__dirname, '../public')
 const port =process.env.PORT || 2750;
@@ -10,7 +14,13 @@ const port =process.env.PORT || 2750;
  //   res.render (`${publicDir}`);
  // })
 app.use (express.static(publiPath));
+io.on("connect",(socket)=>{
+  console.log('New user connected');
+  socket.on("disconnect",()=>{
+    console.log("User was disconnected!");
+  })
+});
 
-app.listen(port, ()=>{
-  console.log(`You express server is running on ${port}`);
+server.listen(port, ()=>{
+  console.log(`Your express server is running on ${port}`);
 });
